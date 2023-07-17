7a80adb355db239f40d4a01524cc93cbcb7c9b8a
娇子 <9478911+pride-Jiaozi@user.noreply.gitee.com>
2023-07-17 15:06:12 +0800

    获得division组合

--- examboot/src/main/java/com/packages/controller/CustomerController.java
+++ examboot/src/main/java/com/packages/controller/CustomerController.java
@@ -1,9 +1,10 @@
 package com.packages.controller;
 
-import com.packages.entity.Customer;
 import com.packages.service.CustomerService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -11,5 +12,10 @@ import java.util.*;
 @RequestMapping("/customer")
 @RestController
 public class CustomerController {
-
+    @Resource
+    private CustomerService customerService;
+    @GetMapping("/getDivisionCombination")
+    public List<Map<String, Object>> getDivisionCombination() {
+        return customerService.divisionCombination();
+    }
 }
--- examboot/src/main/java/com/packages/service/CustomerService.java
+++ examboot/src/main/java/com/packages/service/CustomerService.java
@@ -1,6 +1,23 @@
 package com.packages.service;
+import com.packages.utils.QueryUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class CustomerService{
+    private final JdbcTemplate jdbcTemplate;
+    public CustomerService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    public List<Map<String, Object>> divisionCombination() {
+        String sql = "SELECT DISTINCT sales_org, distr_channel, division\n" +
+                "FROM sale_area\n" +
+                "GROUP BY sales_org, distr_channel, division;";
+        RowMapper<Map<String, Object>> rowMapper = QueryUtils.genericRowMapper();
+        return jdbcTemplate.query(sql, rowMapper);
+    }
 }
