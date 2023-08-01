396619926e40019af02815e47748f87cbb746b74
Frank_Wuuu <2624744810@qq.com>
2023-08-01 23:33:08 +0800

    补注解2

--- examboot/src/main/java/com/packages/entity/MaterialSd.java
+++ examboot/src/main/java/com/packages/entity/MaterialSd.java
@@ -5,16 +5,16 @@ import lombok.Data;
 @Data
 public class MaterialSd {
     @TableId(type = IdType.AUTO)
-    private Integer msdid;
-    private String mid;
-    private String salesorg;
-    private String distrchannel;
-    private String currency;
+    private Integer msdid;  //主键，标识该物料
+    private String mid; //物料编号
+    private String salesorg;    //销售组织
+    private String distrchannel;    //分销渠道
+    private String currency;    //货币
     private Double price;
-    private String division;
+    private String division;    //分区
     private String delstorplant;
-    private String storageloc;
-    private String descrp;
+    private String storageloc;  //储存位置
+    private String descrp;  //形容
     private String bomunit;
     private Double weight;
     private String weightunit;
