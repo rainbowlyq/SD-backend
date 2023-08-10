68acc4ee546194389e041cf3587cb39a82fe0354
Maraschino1212 <1571248254@qq.com>
2023-08-10 19:52:28 +0800

    fix bug

--- examboot/src/main/java/com/packages/service/DeliveryService.java
+++ examboot/src/main/java/com/packages/service/DeliveryService.java
@@ -2,12 +2,10 @@ package com.packages.service;
 
 import com.packages.entity.Delivery;
 import com.packages.entity.Salesorder;
-import com.packages.entity.Sell;
 import com.packages.mapper.DeliveryMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -25,11 +23,6 @@ public class DeliveryService extends BaseService<DeliveryMapper, Delivery> {
     @Override
     @Transactional
     public boolean createOrUpdate(Delivery delivery) {
-        List<Sell> items = delivery.getItems();
-        if (CollectionUtils.isEmpty(items)) {
-            return false;
-        }
-
         Integer salordid = delivery.getSalordid();
         Salesorder saleOrder = salesOrderService.getBySalordId(salordid);
         delivery.setShiptoparty(saleOrder.getShiptoparty());
